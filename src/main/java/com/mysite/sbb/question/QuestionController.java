235e270f7f21b7c2b6045e5ecb34dd44d176b1ba
yi-hyuck <mhyy020@gmail.com>
2025-11-10 13:59:47 +0900

    3-05

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,10 +25,10 @@ public class QuestionController {
 	private final QuestionService questionService;
 	
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Question> questionList = this.questionService.getList();
+	public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
 		
-		model.addAttribute("questionList", questionList);
+		Page<Question> paging = this.questionService.getList(page);
+		model.addAttribute("paging", paging);
 		
 		return "question_list";
 	}
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,11 +1,15 @@
 package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import com.mysite.sbb.DataNotFoundException;
 
-import org.springframework.boot.context.config.ConfigDataLocationNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -40,5 +44,15 @@ public class QuestionService {
 		q.setCreateDate(LocalDateTime.now());
 		
 		this.questionRepository.save(q);
+	}
+	
+	public Page<Question> getList(int page) {
+		
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));
+		
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+		
+		return this.questionRepository.findAll(pageable);
 	}
 }
