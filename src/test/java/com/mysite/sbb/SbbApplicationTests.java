1d4d876996d769f4260c38d035a30972e3f101d6
yi-hyuck <mhyy020@gmail.com>
2025-11-10 11:31:22 +0900

    2-07

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -11,6 +11,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
@@ -25,6 +30,7 @@ class SbbApplicationTests {
 	@Transactional
     @Test
     void testJpa() {
+		
         Optional<Question> oq = this.questionRepository.findById(2);
         assertTrue(oq.isPresent());
         Question q = oq.get();
