ab639799d7052bf9c3d118a4b8cbc1f0015f996f
yi-hyuck <mhyy020@gmail.com>
2025-11-10 16:44:46 +0900

    3-13

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Component;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+@Component
+public class CommonUtil {
+	
+	public String markdown(String markdown) {
+		Parser parser = Parser.builder().build();
+		Node document = parser.parse(markdown);
+		HtmlRenderer renderer = HtmlRenderer.builder().build();
+		
+		return renderer.render(document);
+	}
+}
--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -44,9 +44,9 @@ public class AnswerController {
 			return "question_detail";
 		}
 	
-		this.answerService.create(question, answerForm.getContent(), siteUser);
+		Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
 		
-		return String.format("redirect:/question/detail/%s", id);
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 	
 	@PreAuthorize("isAuthenticated()")
@@ -81,7 +81,7 @@ public class AnswerController {
 		
 		this.answerService.modify(answer, answerForm.getContent());
 		
-		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 	
 	@PreAuthorize("isAuthenticated()")
@@ -106,6 +106,6 @@ public class AnswerController {
 		Answer answer = this.answerService.getAnser(id);
 		SiteUser siteUser = this.userService.getUser(principal.getName());
 		this.answerService.vote(answer, siteUser);
-		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 }
